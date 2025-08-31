3f16bca18d06eebd51c052921bb1078d4fb53a5f
Md Shamoel Farooqui <farooquishamoel@gmail.com>
2025-08-31 23:59:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://vibely-frontend-rdhy.onrender.com",
     credentials:true
 }))
 app.use(express.json())
